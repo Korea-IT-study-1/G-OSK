3828bcdcb49f2f748a7a23aa919773eef400cdbd
wnsgh2739 <wnsgh2739@naver.com>
2022-12-23 17:47:19 +0900

    20221223-준호

---
+++ src/main/resources/static/js/seat_check.js
@@ -0,0 +1,26 @@
+const seatbasic = document.querySelector(".seat-basic");
+const seatspecial = document.querySelector(".seat-special");
+const basic = document.querySelector(".basic");
+const special = document.querySelector(".special");
+const btn = document.querySelectorAll(".btn1");
+
+special.onclick =() => {
+    seatspecial.classList.remove("invisible");
+    seatbasic.classList.add("invisible");
+    basic.classList.remove("sky-btn");
+    special.classList.add("sky-btn");
+}
+basic.onclick =() => {
+    seatbasic.classList.remove("invisible");
+    seatspecial.classList.add("invisible");
+    special.classList.remove("sky-btn");
+    basic.classList.add("sky-btn");
+}
+
+$(".seat-content button").click(function(){
+    if($(this).hasClass('sky-btn') == true){
+        $(this).removeClass('sky-btn');
+    }else{
+        $(this).addClass('sky-btn').siblings().removeClass('sky-btn');
+    }
+})
