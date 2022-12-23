0b1780b16c2cf7cf089d252e60a781d35b2b454c
buzz9248 <buzz9248@naver.com>
2022-12-23 16:42:32 +0900

    2022-12-23

--- src/main/resources/static/js/adminpage/product-manage.js
+++ src/main/resources/static/js/adminpage/product-manage.js
@@ -47,3 +47,13 @@ popup2UpdateBtn.onclick = () => {
 
 const reserved = document.querySelector(".reserved");
 const nomal = document.querySelector(".nomal");
+
+// 지정석만 조회
+reserved.onclick = () => {
+    alert("지정석");
+}
+
+// 일반석만 조회
+nomal.onclick = () => {
+    alert("일반석");
+}
---
+++ src/main/resources/static/js/adminpage/seat-manage.js
@@ -0,0 +1,12 @@
+const reserved = document.querySelector(".reserved");
+const nomal = document.querySelector(".nomal");
+
+// 지정석만 조회
+reserved.onclick = () => {
+    alert("지정석");
+}
+
+// 일반석만 조회
+nomal.onclick = () => {
+    alert("일반석");
+}
