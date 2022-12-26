c5c7096a8480ec01c8ec4efe921807dde52629cc
조창석 <yulini1022@gmail.com>
2022-12-26 11:12:57 +0900

    사물함 선택 js 추가 및 css 수정

--- src/main/resources/static/js/seat_check.js
+++ src/main/resources/static/js/seat_check.js
@@ -2,8 +2,8 @@ const seatbasic = document.querySelector(".seat-basic");
 const seatspecial = document.querySelector(".seat-special");
 const basic = document.querySelector(".basic");
 const special = document.querySelector(".special");
-const btn = document.querySelectorAll(".btn1");
 
+//일반석,지정석 변경버튼 클릭 이벤트
 special.onclick =() => {
     seatspecial.classList.remove("invisible");
     seatbasic.classList.add("invisible");
@@ -17,13 +17,19 @@ basic.onclick =() => {
     basic.classList.add("sky-btn");
 }
 
+//선택한 좌석 클릭 시 버튼색깔 변경 및 좌석 이름 표시
 $(".seat-content button").click(function(){
-    if($(this).hasClass('sky-btn') == true){
-        $(this).removeClass('sky-btn');
-        $(".locker-select-name").attr('value',"");
-    }else{
+    if($(this).hasClass('org-btn') == false){
+        if($(this).hasClass('sky-btn') == true){
+            $(this).removeClass('sky-btn');
+            $(".seat-select-name").attr('value',"");
+        }else{
+            $(this).addClass('sky-btn').siblings().removeClass('sky-btn');
+            $('.seat-select-name').attr('value',$(this).text());
+        }
+    } else {
         $(this).addClass('sky-btn').siblings().removeClass('sky-btn');
-        $('.locker-select-name').attr('value',$(this).text());
+        $(".seat-select-name").attr('value',"");
     }
 })
 
