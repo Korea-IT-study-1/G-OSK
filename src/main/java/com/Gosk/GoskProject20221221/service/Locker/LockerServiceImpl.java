aca9a014576703d71f9b333ebdebf26909d98773
wnsgh2739 <wnsgh2739@naver.com>
2022-12-28 16:17:04 +0900

    20221228-입실/퇴실중

--- src/main/java/com/Gosk/GoskProject20221221/service/Locker/LockerServiceImpl.java
+++ src/main/java/com/Gosk/GoskProject20221221/service/Locker/LockerServiceImpl.java
@@ -1,17 +1,17 @@
-package com.Gosk.GoskProject20221221.service.Locker;
-
-import com.Gosk.GoskProject20221221.dto.Loker.LockerReqDto;
-import com.Gosk.GoskProject20221221.repository.Loker.LockerRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class LockerServiceImpl {
-
-    private final LockerRepository lockerRepository;
-//  -------------------입실/퇴실---------------------------
-    public boolean updateLocker(LockerReqDto lockerReqDto)throws Exception{
-        return lockerRepository.updateLocker(lockerReqDto.toLockerEntity(principal))>0;
-    }
-}
+//package com.Gosk.GoskProject20221221.service.Locker;
+//
+//import com.Gosk.GoskProject20221221.dto.Loker.LockerReqDto;
+//import com.Gosk.GoskProject20221221.repository.Loker.LockerRepository;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//@RequiredArgsConstructor
+//public class LockerServiceImpl {
+//
+////    private final LockerRepository lockerRepository;
+//////  -------------------입실/퇴실---------------------------
+////    public boolean updateLocker(LockerReqDto lockerReqDto)throws Exception{
+////        return lockerRepository.updateLocker(lockerReqDto.toLockerEntity(principal)) > true;
+////    }
+//}
