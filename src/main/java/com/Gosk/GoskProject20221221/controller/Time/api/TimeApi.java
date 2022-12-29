6265a46f98d3278682c19f26492eda27fc8c81d0
조창석 <yulini1022@gmail.com>
2022-12-29 16:43:00 +0900

    원데이 이용권 불러오기 js

--- src/main/java/com/Gosk/GoskProject20221221/controller/Time/api/TimeApi.java
+++ src/main/java/com/Gosk/GoskProject20221221/controller/Time/api/TimeApi.java
@@ -3,6 +3,9 @@ package com.Gosk.GoskProject20221221.controller.Time.api;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class TimeApi {
+
+//    @GetMapping("/list")
+//    public ResponseEntity<?> getList() throws Exception{
+////        if(principalDetails == null){
+////            return ResponseEntity.ok().body(new CMRespDto<>(1, "Successfully", bagService.getBagList(0)));
+////        }
+//
+////        User user = principalDetails.getUser();
+////        log.info("{}", bagService.getBagList(user.getId()));
+////        return ResponseEntity.ok().body(new CMRespDto<>(1, "Successfully", bagService.getBagList(user.getId())));
+//    }
+
+
 }
--- src/main/resources/static/js/time_oneday.js
+++ src/main/resources/static/js/time_oneday.js
@@ -10,3 +10,59 @@ $('.time-content li').click(function(){
     localStorage.setItem("pay", $(this).children('div').text());
     location.href = "/pay";
 });
+
+//페이지 실행 시 원데이이용권 리스트 불러오기
+class OnedayService{
+    // #responseData = null;
+
+    loadOnedayList(){
+        this.responseData = OnedayApi.getOneday();
+        console.log(this.responseData);
+        this.getOnedayList(this.responseData);
+    }
+
+    getOnedayList(responseData){
+        console.log(responseData)
+        const borders = document.querySelector(".oneday-list");
+
+        responseData.forEach((border, index) => {
+
+            borders.innerHTML += `
+            <li>
+                <p>
+                    <i class="fa-solid fa-ticket"></i>&nbsp&nbsp&nbsp&nbsp&nbsp
+                    <span>"${border.oneday_time}"시간</span>
+                </p>
+                <div>"${border.oneday_price}"원</div>
+            </li>
+            `;
+
+        });
+    }
+}
+
+class OnedayApi{
+    getOneday(){
+
+        let responseData = null;
+        
+        $.ajax({
+            async: false,
+            type: "get",
+            url: "/api/api/time/list",
+            dataType: "json",
+            success: (response) => {
+                responseData = response.data;
+            },
+            error: (error) => {
+                console.log(error);
+            }
+        });
+        
+        return responseData;
+    }
+}
+
+window.onload = () => {
+    OnedayService.loadOnedayList();
+}
