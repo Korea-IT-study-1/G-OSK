5d8887d236f0d70e49f43413da18efd438fb8bb1
buzz9248 <buzz9248@naver.com>
2022-12-29 16:40:00 +0900

    도메인 user

--- src/main/java/com/Gosk/GoskProject20221221/domain/User.java
+++ src/main/java/com/Gosk/GoskProject20221221/domain/User.java
@@ -15,15 +15,16 @@ import java.time.LocalTime;
 public class User {
     private int user_id;
     private int role_id;
-    private int seat_num;
-    private int reserved_num;
+    private String seat_num;
+    private String reserved_num;
     private String locker_num;
     private String user_phone;
     private String user_pw;
-    private String role;
     private LocalTime user_time;
     private LocalTime user_date;
     private LocalTime user_locker_date;
     private LocalDateTime user_create_date;
     private LocalDateTime user_update_date;
+    private LocalDateTime receipt_start_date;
+    private LocalDateTime receipt_end_date;
 }
