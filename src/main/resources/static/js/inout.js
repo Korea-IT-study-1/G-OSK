5f8a631a22babe77c9d01ae54de3e9c4c2f37717
buzz9248 <buzz9248@naver.com>
2022-12-30 13:48:18 +0900

    inout.js 로그아웃

--- src/main/resources/static/js/inout.js
+++ src/main/resources/static/js/inout.js
@@ -154,8 +154,9 @@ window.onload = () => {
     // console.log(localStorage.getItem("address"))
 };
 
-//홈으로 버튼 이벤트
-$('.home-btn').click(function () {
-    localStorage.clear();
-    location.replace("/index");
-});
+const logOutBtn = document.querySelector(".logout-btn");
+
+logOutBtn.onclick = () => {
+    console.log("로그아웃")
+    location.replace("/logout");
+}
