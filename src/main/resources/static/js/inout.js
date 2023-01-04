2a03c0113fc1729d920d2ae77996dd8bd4ccde31
wnsgh2739 <wnsgh2739@naver.com>
2023-01-04 18:01:10 +0900

    20230104-준호

--- src/main/resources/static/js/inout.js
+++ src/main/resources/static/js/inout.js
@@ -5,24 +5,10 @@ function inList() {
         url: "/api/inout",
         dataType: "json",
         success: (response) => {
-                if (localStorage.getItem("time") == "in") {
-                    alert("입실 in성공");
-                    responseData1 = response.data;
-                    responseData = responseData1[responseData1.length - 1];
-                    console.log(responseData);
-                    ininList(responseData);
-                } else if (localStorage.getItem("time") == "out") {
-                    responseData1 = response.data;
-                    responseData = responseData1[responseData1.length - 1];
-                    timetime =responseData.user_update_date.replace(/T/g, ' ');
-                    console.log(timetime);
-                    timetime = timetime.split(/[.]/,1);
-                    console.log(timetime);
-                    console.log(responseData);
-                    alert("퇴실in 성공");
-                    outload(responseData);
-                    
-                }
+            responseData1 = response.data;
+            responseData = responseData1[responseData1.length - 1];
+            console.log(responseData);
+            ininList(responseData);
 
         },
         error: (error) => {
@@ -80,14 +66,14 @@ function ininList() {
     console.log(time3);
     timehour = junho4 + time1;
     timeminute = junho5 + time2;
-    timesecond = junho6 +  time3;
+    timesecond = junho6 + time3;
     console.log(timehour);
     console.log(timeminute);
     console.log(timesecond);
-    let goodtime ={
-        timehour : time1,
-        timeminute : time2,
-        timesecond : time3,
+    let goodtime = {
+        timehour: time1,
+        timeminute: time2,
+        timesecond: time3,
     };
     console.log(goodtime);
     junholist(goodtime);
@@ -105,7 +91,7 @@ function junholist(goodtime) {
         success: (response) => {
             alert("시간 보내기 성공");
             responseData2 = response.data.replace(/T/g, ' ');
-            responseData2 = responseData2.split(/[.]/,1);
+            responseData2 = responseData2.split(/[.]/, 1);
             console.log(responseData2);
             inininList();
         },
@@ -127,18 +113,20 @@ function inininList() {
 
             responseData1 = response.data;
             responseData = responseData1[responseData1.length - 1];
-            timetime =responseData.user_update_date.replace(/T/g, ' ');
+            timetime = responseData.user_update_date.replace(/T/g, ' ');
             console.log(timetime);
-            timetime = timetime.split(/[.]/,1);
+            timetime = timetime.split(/[.]/, 1);
+            week = responseData.user_date.replace(/T/g, ' ');
+            console.log(timetime);
+            week = week.split(/[.]/, 1);
             console.log(timetime);
             console.log(responseData);
             if (localStorage.getItem("time") == "in") {
                 alert("입실입실성공");
-
                 inload(responseData);
             } else if (localStorage.getItem("time") == "out") {
                 alert("퇴실성공");
-                
+
                 outload(responseData);
             }
         },
@@ -150,9 +138,39 @@ function inininList() {
 }
 
 function inload(responseData) {
-
+    // onsole.log(responseData);
     const inoutBody = document.querySelector(".junho11");
-    inoutBody.innerHTML += `
+    if (responseData.receipt_time == 0) {
+        inoutBody.innerHTML += `
+        <header class="inout-header">
+        <i class="fa-solid fa-check"></i>
+        <p>입실처리 완료</p>
+        <div>회원님, 처리가 입실처리가 완료되었습니다.</div>
+        </header>
+
+        <section class="inout-content">
+            <ul>
+                <li>
+                    <p><i class="fa-solid fa-hourglass-half"></i>구매권</p>
+                    <span>${responseData.receipt_day}주</span>
+                </li>
+                <li>
+                    <p><i class="fa-regular fa-clock"></i>입실(현재)시간</p>
+                    <span>${timetime}</span>
+                </li>
+                <li class="close">
+                    <p><i class="fa-regular fa-calendar-xmark"></i>종료일자</p>
+                    <span>${week}<span>
+                </li>
+                <li>
+                    <p><i class="fa-solid fa-chair"></i>입석좌석</p>
+                    <div>${responseData.seat_id}</div>
+                </li>
+            </ul>
+        </section>
+            `
+    }else if(responseData.receipt_time != 0){
+        inoutBody.innerHTML += `
         <header class="inout-header">
         <i class="fa-solid fa-check"></i>
         <p>입실처리 완료</p>
@@ -180,6 +198,8 @@ function inload(responseData) {
             </ul>
         </section>
             `
+    }
+    
 }
 
 function outload(responseData) {
@@ -207,7 +227,7 @@ function outload(responseData) {
                 </li>
                 <li class="close">
                     <p><i class="fa-regular fa-calendar-xmark"></i>종료일자</p>
-                    <span>${responseData.user_date}<span>
+                    <span>${week}<span>
                 </li>
                 <li>
                     <p><i class="fa-solid fa-chair"></i>퇴실좌석</p>
