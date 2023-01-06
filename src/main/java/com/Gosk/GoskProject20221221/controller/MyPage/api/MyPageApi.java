0b6ebfc5b811d8f4c685def6493f96aa823786d9
wnsgh2739 <wnsgh2739@naver.com>
2023-01-06 13:01:03 +0900

    준호

--- src/main/java/com/Gosk/GoskProject20221221/controller/MyPage/api/MyPageApi.java
+++ src/main/java/com/Gosk/GoskProject20221221/controller/MyPage/api/MyPageApi.java
@@ -1,8 +1,14 @@
 package com.Gosk.GoskProject20221221.controller.MyPage.api;
 
 
+import com.Gosk.GoskProject20221221.dto.CMRespDto;
+import com.Gosk.GoskProject20221221.service.MyPage.MyPageService;
+import com.Gosk.GoskProject20221221.service.auth.PrincipalDetails;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequiredArgsConstructor
 public class MyPageApi {
+    private final MyPageService myPageService;
+    @GetMapping("/receipt")
+    public ResponseEntity<?> receipt(@AuthenticationPrincipal PrincipalDetails principalDetails) throws Exception {
+        return ResponseEntity.ok(new CMRespDto<>(1,"scuccess",myPageService.receipt(principalDetails.getUser().getUser_id())));
+    }
 }
---
+++ src/main/java/com/Gosk/GoskProject20221221/repository/MyPageRepository.java
@@ -0,0 +1,12 @@
+package com.Gosk.GoskProject20221221.repository;
+
+import com.Gosk.GoskProject20221221.domain.MyPage;
+import com.Gosk.GoskProject20221221.domain.Seat;
+import com.Gosk.GoskProject20221221.domain.User;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+@Mapper
+public interface MyPageRepository {
+    public List<MyPage> receipt(int user_id) throws Exception;
+}
